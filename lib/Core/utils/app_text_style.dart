c143d9bdc69e7d455c27e84aecc71702753b471a
EsAm1973 <me354172@gmail.com>
2025-10-08 15:21:09 +0300

    feat: add AppTextStyles class with various text styles for consistent typography

---
+++ lib/Core/utils/app_text_style.dart
@@ -0,0 +1,112 @@
+import 'dart:ui';
+
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+abstract class AppTextStyles {
+  // bold
+  static TextStyle bold15 = TextStyle(
+    fontSize: 15.sp,
+    fontWeight: FontWeight.w600,
+    letterSpacing: -0.27.sp,
+  );
+
+  // semiBold
+  static TextStyle semiBold40 = TextStyle(
+    fontSize: 40.sp,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle semiBold32 = TextStyle(
+    fontSize: 32.sp,
+    fontWeight: FontWeight.w500,
+    letterSpacing: -0.29.sp,
+  );
+
+  static TextStyle semiBold30 = TextStyle(
+    fontSize: 30.sp,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle semiBold28 = TextStyle(
+    fontSize: 28.sp,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle semiBold24 = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle semiBold20 = TextStyle(
+    fontSize: 20.sp,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle semiBold18 = TextStyle(
+    fontSize: 18.sp,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle semiBold16 = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle semiBold15 = TextStyle(
+    fontSize: 15.sp,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle semiBold14 = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w500,
+    letterSpacing: -0.30.sp,
+  );
+
+  // regular
+  static TextStyle regular36 = TextStyle(
+    fontSize: 36.sp,
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle regular18 = TextStyle(
+    fontSize: 18.sp,
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle regular16 = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle regular15 = TextStyle(
+    fontSize: 15.04,
+    fontWeight: FontWeight.w400,
+    height: 1.73.h,
+  );
+
+  static TextStyle regular14 = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w400,
+    letterSpacing: -0.30.sp,
+  );
+
+  static TextStyle regular13 = TextStyle(
+    fontSize: 13.28.sp,
+    fontFamily: 'Almarai',
+    fontWeight: FontWeight.w400,
+    letterSpacing: -0.28.sp,
+  );
+
+  static TextStyle regular12 = TextStyle(
+    fontSize: 12.sp,
+    fontWeight: FontWeight.w400,
+    letterSpacing: -0.30.sp,
+  );
+
+  static TextStyle regular9 = TextStyle(
+    fontSize: 9.sp,
+    fontWeight: FontWeight.w400,
+    letterSpacing: -0.30.sp,
+  );
+}
